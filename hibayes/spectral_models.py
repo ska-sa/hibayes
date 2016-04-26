1d6c0578af503930c6a52051dd660ba0d7cf3331
jtlz2 <jtlz2@astro.columbia.edu>
2016-04-26 12:31:30 +0200

    tidying

--- hibayes/spectral_models.py
+++ hibayes/spectral_models.py
@@ -1,7 +1,6 @@
 """
 # spectral_models.py
 
-
 """
 import os
 import sys
@@ -13,6 +12,7 @@ from .priors import Priors
 from .profile_support import profile
 from .constants import *
 
+#-------------------------------------------------------------------------------
 
 @profile
 def T_fg(nu_1, c, nc, nu):
@@ -30,9 +30,6 @@ def T_fg(nu_1, c, nc, nu):
     #exponent=c[0]+c[1]*(numpy.log10(nu/nu_1))**1
 
     Tf = 10 ** exponent
-    #Tf = exp(exponent)
-    #Tf=exponent
-    #print Tf
 
     return Tf
 
@@ -42,26 +39,18 @@ def T_fg(nu_1, c, nc, nu):
 @profile
 def T_HI(A_HI, nu_HI, sigma_HI, nu, norm=False, erfs=False):
     """
-    Units are MHz and ***mK***
+    Units are MHz and mK
     """
 
     T_HI = A_HI * exp(-(nu - nu_HI) ** 2 / (2.0 * sigma_HI ** 2))
-    #T_HI *= 1.0/sqrt(2.0*pi*sigma_HI**2) # Found not required for 100, 67, 5
 
-    #print nu,nu_HI,erfs
-    #return T_HI
     if norm: T_HI *= 1.0 / sqrt(2.0 * pi * sigma_HI ** 2)
     if erfs:
-        ##    erfs=0.5*(erf((nu_HI-FREQ_MIN)/(sqrtTwo*sigma_HI)) - erf((nu_HI-FREQ_MAX)/(sqrtTwo*sigma_HI))) # !!!! WORKS FOR 100, 67, 5
-        #erfs=0.5*(erf(nu_HI/(sqrtTwo*sigma_HI)) + erf((FREQ_MAX-FREQ_MIN-nu_HI)/(sqrtTwo*sigma_HI)))
         erfs = 0.5 * (erf((FREQ_MAX - nu_HI) / (sqrtTwo * sigma_HI)) - erf(
-            (FREQ_MIN - nu_HI) / (sqrtTwo * sigma_HI)))  #*sqrt(2.0*pi*sigma_HI**2)
+            (FREQ_MIN - nu_HI) / (sqrtTwo * sigma_HI)))
         T_HI *= erfs
 
     return T_HI
-    #return T_HI/erfs
-    #return erfs*T_HI # !!!! WORKS FOR 100, 67, 5
-
 
 #-------------------------------------------------------------------------------
 
@@ -73,6 +62,7 @@ def sigma(Tsky, B, t):
     """
 
     sigma = Tsky / sqrt(B * t)
-    #sigma=1.0e-9
 
     return sigma
+
+#-------------------------------------------------------------------------------
